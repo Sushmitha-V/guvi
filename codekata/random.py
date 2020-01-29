3ab51064e966d55f34073f0148dee1a99c986bac
Sushmitha-V <60400955+Sushmitha-V@users.noreply.github.com>
2020-01-29 15:36:07 +0530

    Create random.py

---
+++ codekata/random.py
@@ -0,0 +1,9 @@
+from random import randint
+value = randint (0,5)
+print ("enter a number")
+a=int(input())
+if(value==a):
+ print("success")
+else:
+  print("failure") 
+print("the random value is",value)
